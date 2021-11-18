48b62afb77e7e206aad6bbf5c9b23d29b2fec5ed
Michael Benayoun <mickbenayoun@gmail.com>
2021-11-18 18:00:43 +0100

    [WIP] IPUTrainer disentanglement from original Trainer

--- optimum/graphcore/modeling_utils.py
+++ optimum/graphcore/modeling_utils.py
@@ -14,6 +14,8 @@
 
 import copy
 
+from torch import nn
+
 from transformers import PreTrainedModel
 
 from .ipu_configuration import IPUConfig
@@ -70,3 +72,30 @@ class PipelineMixin:
         original model.
         """
         return self
+
+    def num_parameters(self, only_trainable: bool = False, exclude_embeddings: bool = False) -> int:
+        """
+        Get number of (optionally, trainable or non-embeddings) parameters in the module.
+
+        Args:
+            only_trainable (:obj:`bool`, `optional`, defaults to :obj:`False`):
+                Whether or not to return only the number of trainable parameters
+
+            exclude_embeddings (:obj:`bool`, `optional`, defaults to :obj:`False`):
+                Whether or not to return only the number of non-embeddings parameters
+
+        Returns:
+            :obj:`int`: The number of parameters.
+        """
+
+        # TODO: actually overwrite this to handle SerializedEmbedding.
+        if exclude_embeddings:
+            embedding_param_names = [
+                f"{name}.weight" for name, module_type in self.named_modules() if isinstance(module_type, nn.Embedding)
+            ]
+            non_embedding_parameters = [
+                parameter for name, parameter in self.named_parameters() if name not in embedding_param_names
+            ]
+            return sum(p.numel() for p in non_embedding_parameters if p.requires_grad or not only_trainable)
+        else:
+            return sum(p.numel() for p in self.parameters() if p.requires_grad or not only_trainable)
