fcec7f88f2c871b97fdd854f775a88541ee3c42f
James Briggs <jamesbr@graphcore.ai>
2021-12-13 12:39:20 +0000

    Added Roberta model with base and large configs for question answering

--- optimum/graphcore/models/roberta/__init__.py
+++ optimum/graphcore/models/roberta/__init__.py
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import bert, roberta
+from .modeling_roberta import PipelinedRobertaForQuestionAnswering
---
+++ optimum/graphcore/models/roberta/modeling_roberta.py
@@ -0,0 +1,187 @@
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.nn as nn
+
+import poptorch
+import transformers
+
+from ...modeling_utils import PipelineMixin, register
+from ...utils import logging
+
+
+logger = logging.get_logger(__name__)
+
+
+def _get_layer_ipu(layers_per_ipu):
+    # List of the IPU Id for each encoder layer
+    layer_ipu = []
+    for ipu, n_layers in enumerate(layers_per_ipu):
+        layer_ipu += [ipu] * n_layers
+    return layer_ipu
+
+
+def recomputation_checkpoint(module: nn.Module):
+    """Annotates the output of a module to be checkpointed instead of
+        recomputed"""
+    def recompute_outputs(module, inputs, outputs):
+        return tuple(poptorch.recomputationCheckpoint(y) for y in outputs)
+    module.register_forward_hook(recompute_outputs)
+
+
+def outline_attribute(module: nn.Module, value: str):
+    """Adds an attribute to a module. This attribute will be used
+        when comparing operation equivalence in outlining. For example:
+
+        layer1 = nn.Linear(...)
+        layer2 = nn.Linear(...)
+        layer3 = nn.Linear(...)
+        layer4 = nn.Linear(...)
+        outline_attribute(layer1, "A")
+        outline_attribute(layer2, "A")
+        outline_attribute(layer3, "B")
+
+        The code for layer1 can be reused for layer2.
+        But it can't be used for layer3 or layer4.
+    """
+    context = poptorch.Attribute(__outline={"layer": value})
+
+    def enable(*args):
+        context.__enter__()
+
+    def disable(*args):
+        context.__exit__(None, None, None)
+    module.register_forward_pre_hook(enable)
+    module.register_forward_hook(disable)
+
+
+class SerializedEmbedding(nn.Module):
+    """
+    Wrapper for `nn.Embedding` layer that performs the embedding look-up into
+    smaller serialized steps in order to reduce memory in the embedding gradient
+    calculation.
+
+    Args:
+        embedding: A `nn.Embedding` to wrap
+        serialization_factor: The number of serialized embedding look-ups
+    """
+    def __init__(self, embedding: nn.Embedding, serialization_factor: int):
+        super().__init__()
+        self.serialization_factor = serialization_factor
+        self.num_embeddings = embedding.num_embeddings
+
+        # Num embeddings should be divisible by the serialization factor
+        assert self.num_embeddings % self.serialization_factor == 0
+        self.split_size = self.num_embeddings // self.serialization_factor
+        self.split_embeddings = nn.ModuleList(
+            [nn.Embedding.from_pretrained(embedding.weight[i*self.split_size:(i+1)*self.split_size, :].detach(),
+                                          freeze=False,
+                                          padding_idx=embedding.padding_idx if i == 0 else None)
+             for i in range(self.serialization_factor)])
+
+    def deserialize(self):
+        """
+        Deserialize the internal wrapped embedding layer and return it as a
+        `nn.Embedding` object.
+
+        Returns:
+            `nn.Embedding` layer
+        """
+        return nn.Embedding.from_pretrained(torch.vstack([l.weight for l in self.split_embeddings]), padding_idx=0)
+
+    def forward(self, indices):
+        # iterate through the splits
+        x_sum = None
+        for i in range(self.serialization_factor):
+            # mask out the indices not in this split
+            split_indices = indices - i * self.split_size
+            mask = (split_indices >= 0) * (split_indices < self.split_size)
+            mask = mask.detach()
+            split_indices *= mask
+
+            # do the embedding lookup
+            x = self.split_embeddings[i](split_indices)
+
+            # multiply the output by mask
+            x *= mask.unsqueeze(-1)
+
+            # add to partial
+            if x_sum is not None:
+                x_sum += x
+            else:
+                x_sum = x
+        return x_sum
+
+
+@register(transformers.RobertaForQuestionAnswering)
+class PipelinedRobertaForQuestionAnswering(transformers.RobertaForQuestionAnswering, PipelineMixin):
+    def parallelize(self):
+        """
+        Transform the model to run in an IPU pipeline.
+        - Adds pipeline stages to the model
+        - (If enabled) Replaces the word embedding with a SerializedEmbedding
+        - Adds recomputation checkpoints
+
+        Recommended usage:
+        ```
+        model = PipelinedRobertaForQuestionAnswering(config).parallelize().half()
+        ```
+        """
+        layer_ipu = _get_layer_ipu(self.config.layers_per_ipu)
+
+        logger.info("-------------------- Device Allocation --------------------")
+        logger.info("Embedding  --> IPU 0")
+        if self.config.embedding_serialization_factor > 1:
+            self.roberta.embeddings.word_embeddings = SerializedEmbedding(
+                self.roberta.embeddings.word_embeddings, self.config.embedding_serialization_factor
+            )
+        self.roberta.embeddings = poptorch.BeginBlock(self.roberta.embeddings, "Embedding", ipu_id=0)
+        outline_attribute(self.roberta.embeddings.LayerNorm, "embedding")
+
+        for index, layer in enumerate(self.roberta.encoder.layer):
+            ipu = layer_ipu[index]
+            if self.config.recompute_checkpoint_every_layer and index != self.config.num_hidden_layers - 1:
+                recomputation_checkpoint(layer)
+            self.roberta.encoder.layer[index] = poptorch.BeginBlock(layer, f"Encoder{index}", ipu_id=ipu)
+            logger.info(f"Encoder {index:<2} --> IPU {ipu}")
+
+        logger.info(f"QA Outputs --> IPU {ipu}")
+        self.qa_outputs = poptorch.BeginBlock(self.qa_outputs, "QA Outputs", ipu_id=ipu)
+        logger.info("-----------------------------------------------------------")
+        return self
+
+    def deparallelize(self):
+        """
+        Undo the changes to the model done by `parallelize`.
+        You should call this before doing `save_pretrained` so that the `model.state_dict` is
+        fully compatible with `transformers.RobertaForQuestionAnswering`.
+        """
+        # Deserialize the serialized word embedding
+        if self.config.embedding_serialization_factor > 1:
+            self.roberta.embeddings.word_embeddings = self.roberta.embeddings.word_embeddings.deserialize()
+        return self
+
+    def forward(self, input_ids, attention_mask, start_positions=None, end_positions=None):
+        output = super().forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            start_positions=start_positions,
+            end_positions=end_positions,
+        )
+        if self.training:
+            final_loss = poptorch.identity_loss(output.loss, reduction="none")
+            return final_loss, output.start_logits, output.end_logits
+        else:
+            return output.start_logits, output.end_logits
