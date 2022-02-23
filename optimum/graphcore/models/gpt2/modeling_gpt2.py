bc20d69e3654db872ccc1029313062ebaa1e8864
Jinchen Ge <jincheng@graphcore.ai>
2022-02-23 12:26:36 +0000

    Add SerializedEmbedding and GPT2PipelineMixin

--- optimum/graphcore/models/gpt2/modeling_gpt2.py
+++ optimum/graphcore/models/gpt2/modeling_gpt2.py
@@ -113,6 +113,112 @@ def outline_attribute(module: nn.Module, value: str):
     return h1, h2
 
 
+class SerializedEmbedding(nn.Module):
+    """
+    Wrapper for `nn.Embedding` layer that performs the embedding look-up into
+    smaller serialized steps in order to reduce memory in the embedding gradient
+    calculation.
+    Args:
+        embedding: A `nn.Embedding` to wrap
+        serialization_factor: The number of serialized embedding look-ups
+    """
+
+    def __init__(self, embedding: nn.Embedding, serialization_factor: int):
+        super().__init__()
+        self.serialization_factor = serialization_factor
+        self.num_embeddings = embedding.num_embeddings
+
+        # Num embeddings should be divisible by the serialization factor
+        assert self.num_embeddings % self.serialization_factor == 0
+        self.split_size = self.num_embeddings // self.serialization_factor
+        self.split_embeddings = nn.ModuleList(
+            [
+                nn.Embedding.from_pretrained(
+                    embedding.weight[i * self.split_size : (i + 1) * self.split_size, :].detach(),
+                    freeze=False,
+                    padding_idx=embedding.padding_idx if i == 0 else None,
+                )
+                for i in range(self.serialization_factor)
+            ]
+        )
+
+    def deserialize(self):
+        """
+        Deserialize the internal wrapped embedding layer and return it as a
+        `nn.Embedding` object.
+        Returns:
+            `nn.Embedding` layer
+        """
+        return nn.Embedding.from_pretrained(torch.vstack([l.weight for l in self.split_embeddings]), padding_idx=0)
+
+    def forward(self, indices):
+        # iterate through the splits
+        x_sum = None
+        for i in range(self.serialization_factor):
+            # mask out the indices not in this split
+            split_indices = indices - i * self.split_size
+            mask = (split_indices >= 0) * (split_indices < self.split_size)
+            mask = mask.detach()
+            split_indices *= mask
+
+            # do the embedding lookup
+            x = self.split_embeddings[i](split_indices)
+
+            # multiply the output by mask
+            x *= mask.unsqueeze(-1)
+
+            # add to partial
+            if x_sum is not None:
+                x_sum += x
+            else:
+                x_sum = x
+        return x_sum
+
+
+class GPT2PipelineMixin(PipelineMixin):
+    def parallelize(self):
+        """
+        Transform the GPT2 model body to run in an IPU pipeline.
+        - Adds pipeline stages to the model
+        - (If enabled) Replaces the word embedding with a SerializedEmbedding
+        - Adds recomputation checkpoints
+        """
+        super().parallelize()
+        layer_ipu = _get_layer_ipu(self.config.layers_per_ipu)
+
+        logger.info("-------------------- Device Allocation --------------------")
+        logger.info("Embedding  --> IPU 0")
+        if self.config.embedding_serialization_factor > 1:
+            self.transformer.wte = SerializedEmbedding(
+                self.transformer.wte, self.config.embedding_serialization_factor
+            )
+        self.transformer.wte = poptorch.BeginBlock(self.transformer.wte, "Token embedding", ipu_id=0)
+        self.transformer.wpe = poptorch.BeginBlock(self.transformer.wpe, "Position embedding", ipu_id=0)
+        hs = outline_attribute(self.transformer.ln_f, "LayerNorm")
+        self._hooks.extend(hs)
+
+        for index, layer in enumerate(self.transformer.h):
+            ipu = layer_ipu[index]
+            if self.config.recompute_checkpoint_every_layer and index != self.config.num_hidden_layers - 1:
+                h = recomputation_checkpoint(layer)
+                self._hooks.append(h)
+            self.transformer.h[index] = poptorch.BeginBlock(layer, f"Layer{index}", ipu_id=ipu)
+            logger.info(f"Layer {index:<2}   --> IPU {ipu}")
+        return self
+
+    def deparallelize(self):
+        """
+        Undo the changes to the model done by `parallelize`.
+        You should call this before doing `save_pretrained` so that the `model.state_dict` is
+        fully compatible with `transformers` models.
+        """
+        super().deparallelize()
+        # Deserialize the serialized word embedding
+        if self.config.embedding_serialization_factor > 1:
+            self.transformer.wte = self.transformer.wte.deserialize()
+        return self
+
+
 @register(GPT2LMHeadModel)
 class PipelinedGPT2LMHeadModel(GPT2LMHeadModel, PipelineMixin):
     def parallelize(self):
@@ -150,7 +256,7 @@ class PipelinedGPT2LMHeadModel(GPT2LMHeadModel, PipelineMixin):
 
         for index, layer in enumerate(self.transformer.h):
             ipu = layer_ipu[index]
-            if self.config.recompute_checkpoint_every_layer:
+            if self.config.recompute_checkpoint_every_layer and index != self.config.num_hidden_layers - 1:
                 h = recomputation_checkpoint(layer)
                 self._hooks.append(h)
             self.transformer.h[index] = poptorch.BeginBlock(layer, f"Layer{index}", ipu_id=ipu)
@@ -180,36 +286,12 @@ class PipelinedGPT2LMHeadModel(GPT2LMHeadModel, PipelineMixin):
 
 
 @register(GPT2ForSequenceClassification)
-class PipelinedGPT2ForSequenceClassification(GPT2ForSequenceClassification, PipelineMixin):
+class PipelinedGPT2ForSequenceClassification(GPT2ForSequenceClassification, GPT2PipelineMixin):
     def parallelize(self):
-        """
-        Transform the model to run in an IPU pipeline.
-        - Adds pipeline stages to the model
-        - Adds recomputation checkpoints
-
-        Recommended usage:
-        ```
-        model = PipelinedGPT2ForSequenceClassification(config).parallelize().half()
-        ```
-        """
-        self._hooks = []
-        layer_ipu = _get_layer_ipu(self.config.layers_per_ipu)
-
-        logger.info("-------------------- Device Allocation --------------------")
-        logger.info("Embedding  --> IPU 0")
-        self.transformer.wte = poptorch.BeginBlock(self.transformer.wte, "Token embedding", ipu_id=0)
-        self.transformer.wpe = poptorch.BeginBlock(self.transformer.wpe, "Position embedding", ipu_id=0)
-
-        for index, layer in enumerate(self.transformer.h):
-            ipu = layer_ipu[index]
-            if self.config.recompute_checkpoint_every_layer:
-                h = recomputation_checkpoint(layer)
-                self._hooks.append(h)
-            self.transformer.h[index] = poptorch.BeginBlock(layer, f"Layer{index}", ipu_id=ipu)
-            logger.info(f"Layer {index:<2}   --> IPU {ipu}")
-
-        print(f"Head       --> IPU {ipu}")
-        self.score = poptorch.BeginBlock(self.score, "Score", ipu_id=ipu)
+        super().parallelize()
+        last_ipu = self.config.ipus_per_replica - 1
+        print(f"Head       --> IPU {last_ipu}")
+        self.score = poptorch.BeginBlock(self.score, "Score", ipu_id=last_ipu)
         logger.info("-----------------------------------------------------------")
         return self
 
@@ -224,36 +306,12 @@ class PipelinedGPT2ForSequenceClassification(GPT2ForSequenceClassification, Pipe
 
 
 @register(GPT2ForTokenClassification)
-class PipelinedGPT2ForTokenClassification(GPT2ForTokenClassification, PipelineMixin):
+class PipelinedGPT2ForTokenClassification(GPT2ForTokenClassification, GPT2PipelineMixin):
     def parallelize(self):
-        """
-        Transform the model to run in an IPU pipeline.
-        - Adds pipeline stages to the model
-        - Adds recomputation checkpoints
-
-        Recommended usage:
-        ```
-        model = PipelinedGPT2ForSequenceClassification(config).parallelize().half()
-        ```
-        """
-        self._hooks = []
-        layer_ipu = _get_layer_ipu(self.config.layers_per_ipu)
-
-        logger.info("-------------------- Device Allocation --------------------")
-        logger.info("Embedding  --> IPU 0")
-        self.transformer.wte = poptorch.BeginBlock(self.transformer.wte, "Token embedding", ipu_id=0)
-        self.transformer.wpe = poptorch.BeginBlock(self.transformer.wpe, "Position embedding", ipu_id=0)
-
-        for index, layer in enumerate(self.transformer.h):
-            ipu = layer_ipu[index]
-            if self.config.recompute_checkpoint_every_layer:
-                h = recomputation_checkpoint(layer)
-                self._hooks.append(h)
-            self.transformer.h[index] = poptorch.BeginBlock(layer, f"Layer{index}", ipu_id=ipu)
-            logger.info(f"Layer {index:<2}   --> IPU {ipu}")
-
-        print(f"Head       --> IPU {ipu}")
-        self.classifier = poptorch.BeginBlock(self.classifier, "Classifier", ipu_id=ipu)
+        super().parallelize()
+        last_ipu = self.config.ipus_per_replica - 1
+        print(f"Head       --> IPU {last_ipu}")
+        self.classifier = poptorch.BeginBlock(self.classifier, "Classifier", ipu_id=last_ipu)
         logger.info("-----------------------------------------------------------")
         return self
 
