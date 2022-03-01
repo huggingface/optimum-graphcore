564ccc0e34e72ef4e91277a9acff8b9176df2f60
Jinchen Ge <jincheng@graphcore.ai>
2022-03-01 16:59:06 +0000

    Style fix

--- examples/question-answering/run_vqa.py
+++ examples/question-answering/run_vqa.py
@@ -423,32 +423,6 @@ def main():
     #     )
     #     data_collator = data_collator_wrapper(data_collator)
 
-    # # Post-processing:
-    # def post_processing_function(examples, features, predictions, stage="eval"):
-    #     # Post-processing: we match the start logits and end logits to answers in the original context.
-    #     predictions = postprocess_qa_predictions(
-    #         examples=examples,
-    #         features=features,
-    #         predictions=predictions,
-    #         version_2_with_negative=data_args.version_2_with_negative,
-    #         n_best_size=data_args.n_best_size,
-    #         max_answer_length=data_args.max_answer_length,
-    #         null_score_diff_threshold=data_args.null_score_diff_threshold,
-    #         output_dir=training_args.output_dir,
-    #         log_level=log_level,
-    #         prefix=stage,
-    #     )
-    #     # Format the result to the format the metric expects.
-    #     if data_args.version_2_with_negative:
-    #         formatted_predictions = [
-    #             {"id": k, "prediction_text": v, "no_answer_probability": 0.0} for k, v in predictions.items()
-    #         ]
-    #     else:
-    #         formatted_predictions = [{"id": k, "prediction_text": v} for k, v in predictions.items()]
-
-    #     references = [{"id": ex["id"], "answers": ex[answer_column_name]} for ex in examples]
-    #     return EvalPrediction(predictions=formatted_predictions, label_ids=references)
-
     # Get the metric function
     metric = load_metric("accuracy")
 
