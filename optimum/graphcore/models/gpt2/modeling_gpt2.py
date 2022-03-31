1595652803de66841c446c2101221bfe20d8b478
Jinchen Ge <jincheng@graphcore.ai>
2022-03-31 11:27:39 +0100

    Remove padding logits before output

--- optimum/graphcore/models/gpt2/modeling_gpt2.py
+++ optimum/graphcore/models/gpt2/modeling_gpt2.py
@@ -186,7 +186,10 @@ class PipelinedGPT2LMHeadModel(GPT2LMHeadModel, PipelineMixin):
             loss_fct = nn.CrossEntropyLoss()
             loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
 
-        output = (lm_logits,) + transformer_outputs[1:]
+        if self.ipu_config.embedding_serialization_factor > 1:
+            output = (lm_logits[:, :, :self.actual_vocab_size],) + transformer_outputs[1:]
+        else:
+            output = (lm_logits,) + transformer_outputs[1:]
         return (loss,) if loss is not None else output
 
 
