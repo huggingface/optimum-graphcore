02af1191cd01634283e92f4910301aef626a8315
Rahul Trada <rahult@graphcore.ai>
2022-04-04 17:38:30 +0100

    Set pipeline using layers_per_ipu from config

--- optimum/graphcore/models/convnext/modeling_convnext.py
+++ optimum/graphcore/models/convnext/modeling_convnext.py
@@ -1,7 +1,9 @@
 import poptorch
 import transformers
+from optimum.utils import logging
 from ...modeling_utils import PipelineMixin, get_layer_ipu, recomputation_checkpoint, register
 
+logger = logging.get_logger(__name__)
 
 @register(transformers.ConvNextModel)
 class PipelinedConvNextModel(transformers.ConvNextModel, PipelineMixin):
@@ -28,11 +30,22 @@ class PipelinedConvNextForImageClassification(transformers.ConvNextForImageClass
         """
         Transform the model to run in an IPU pipeline
         """
+        stages_per_ipu = self.ipu_config.layers_per_ipu
+        num_ipus = len(stages_per_ipu)
+        encoder_stage_on_ipu = get_layer_ipu(stages_per_ipu) 
         super().parallelize()
-        #TODO: Pipeline the model over multiple IPUs in an efficient way
-        #TODO: Add logging (see other models as an example)
+        logger.info("---------- Device Allocation -----------")
+        #TODO: Use recomputation checkpoints? 
         
-        self._add_begin_block(self.classifier, name="Classifier", ipu_id=1)
+        # ConvNext has 4 encoder stages, and ipu_config.layers_per_ipu sets how many of these stages belong to each IPU
+        # e.g. layers_per_ipu = [4] -> all 4 stages on IPU 0. 
+        # e.g. layers_per_ipu = [2,2] -> stages 0 and 1 on IPU0, stages 2 and 3 on IPU1.
+        # The layers before the first encoder stage will belong to the same IPU as the first encoder stage automatically 
+        # The layers after the 4th encoder stage will belong to the same IPU as the 4th encoder stage automatically 
+        for idx, stage_layer in enumerate(self.convnext.encoder.stages):
+            ipu_id = encoder_stage_on_ipu[idx]
+            logger.info(f"Encoder stage {idx} --> IPU {ipu_id}")
+            self._add_begin_block(self.convnext.encoder.stages[idx], name=f"Encoder_stage_{idx}", ipu_id=ipu_id)
         
         return self
     
