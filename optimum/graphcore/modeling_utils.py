69948a0bc216e175822dd605af2f84be996cf6fa
Michael Benayoun <mickbenayoun@gmail.com>
2022-05-03 18:42:04 +0200

    [WIP] generation

--- optimum/graphcore/modeling_utils.py
+++ optimum/graphcore/modeling_utils.py
@@ -159,22 +159,22 @@ class PipelineMixin:
 
 
 class GenerationMethodsMixin:
-    def get_encoder(
-        self,
-        device_iterations: Optional[int] = None,
-        replication_factor: Optional[int] = None,
-        for_inference: bool = True,
-    ):
-        if not hasattr(self, "_wrapped_encoder"):
-            encoder = super().get_encoder()
-            if self.ipu_config.execute_encoder_on_cpu_for_generation:
-                self._wrapped_encoder = encoder.to(torch.float32)
-            else:
-                self.eval_opts = self.ipu_config.to_options(for_inference=True)
-                self._wrapped_encoder = poptorch.inferenceModel(
-                    encoder, options=self.ipu_config.to_options(for_inference=True)
-                )
-        return self._wrapped_encoder
+    # def get_encoder(
+    #     self,
+    #     device_iterations: Optional[int] = None,
+    #     replication_factor: Optional[int] = None,
+    #     for_inference: bool = True,
+    # ):
+    #     if not hasattr(self, "_wrapped_encoder"):
+    #         encoder = super().get_encoder()
+    #         if self.ipu_config.execute_encoder_on_cpu_for_generation:
+    #             self._wrapped_encoder = encoder.to(torch.float32)
+    #         else:
+    #             self.eval_opts = self.ipu_config.to_options(for_inference=True)
+    #             self._wrapped_encoder = poptorch.inferenceModel(
+    #                 encoder, options=self.ipu_config.to_options(for_inference=True)
+    #             )
+    #     return self._wrapped_encoder
 
     def prepare_inputs_for_generation(self, input_ids: torch.LongTensor, **kwargs) -> Dict[str, Any]:
         inputs = super().prepare_inputs_for_generation(input_ids, **kwargs)
