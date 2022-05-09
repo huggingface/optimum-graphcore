850422121ac78ce2e8a4e80105b8d296f23a8ee4
Jinchen Ge <jincheng@graphcore.ai>
2022-05-09 18:04:45 +0100

    Add optimized_gpt2_attn.py

---
+++ optimum/graphcore/models/gpt2/optimized_gpt2_attn.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+from transformers.models.gpt2.modeling_gpt2 import GPT2Attention
+
+
+class OptimizedGPT2Attention(GPT2Attention):
+    def _attn(self, query, key, value, attention_mask=None, head_mask=None):
+        attn_weights = torch.matmul(query, key.transpose(-1, -2))
+
+        if self.scale_attn_weights:
+            attn_weights *= 1.0 / (value.size(-1) ** 0.5)
+
+        # Layer-wise attention scaling
+        if self.scale_attn_by_inverse_layer_idx:
+            attn_weights = attn_weights / float(self.layer_idx + 1)
+
+        if not self.is_cross_attention:
+            # if only "normal" attention layer implements causal mask
+            query_length, key_length = query.size(-2), key.size(-2)
+            causal_mask = self.bias[:, :, key_length - query_length : key_length, :key_length]
+            attn_weights = attn_weights * causal_mask + -1e4 * (1 - causal_mask)
+
+        if attention_mask is not None:
+            # Apply the attention mask
+            attn_weights = attn_weights + attention_mask
+
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
+
+        # Downcast (if necessary) back to V's dtype (if in mixed-precision) -- No-Op otherwise
+        attn_weights = attn_weights.type(value.dtype)
+        attn_weights = self.attn_dropout(attn_weights)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attn_weights = attn_weights * head_mask
+
+        attn_output = torch.matmul(attn_weights, value)
+
+        return attn_output, attn_weights
