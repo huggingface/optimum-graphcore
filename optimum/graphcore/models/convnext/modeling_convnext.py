9278d5becdee62bc57586db7cf59234ed2638c65
Jinchen Ge <jincheng@graphcore.ai>
2022-06-10 16:15:02 +0100

    Remove redundant

--- optimum/graphcore/models/convnext/modeling_convnext.py
+++ optimum/graphcore/models/convnext/modeling_convnext.py
@@ -1,14 +1,23 @@
-import torch
-from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import poptorch
 import transformers
-from transformers.models.convnext import ConvNextModel
-from transformers.modeling_outputs import (
-    ImageClassifierOutputWithNoAttention,
-)
 from optimum.utils import logging
+
 from ...modeling_utils import PipelineMixin, get_layer_ipu, recomputation_checkpoint, register
-from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
+
 
 logger = logging.get_logger(__name__)
 
