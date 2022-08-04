143a7a19087b738155061dcfd36f1e6327a5fe57
Jinchen Ge <gejinchen1996@gmail.com>
2022-08-04 16:05:50 +0100

    Layerdrop for HuBERT (#149)
    Co-authored-by: Jinchen Ge <jincheng@graphcore.ai>

--- examples/audio-classification/run_audio_classification.py
+++ examples/audio-classification/run_audio_classification.py
@@ -303,9 +303,8 @@ def main():
         use_auth_token=True if model_args.use_auth_token else None,
     )
 
-    # Disable these features that can't run on IPU yet
+    # Disable the feature that can't run on IPU yet
     # TODO handle this properly
-    config.layerdrop = 0.0
     config.mask_time_prob = 0.0
 
     ipu_config = IPUConfig.from_pretrained(
