f5193f606a544eacfdf02c0175dd514b16c1c2e3
Jinchen Ge <jincheng@graphcore.ai>
2022-08-04 15:34:14 +0100

    HuBERT use layerdrop

--- optimum/graphcore/models/hubert/modeling_hubert.py
+++ optimum/graphcore/models/hubert/modeling_hubert.py
@@ -15,8 +15,10 @@
 import poptorch
 from optimum.utils import logging
 from transformers import HubertForSequenceClassification
+from transformers.models.hubert.modeling_hubert import HubertEncoder, HubertEncoderStableLayerNorm
 
 from ...modeling_utils import PipelineMixin, get_layer_ipu, recomputation_checkpoint, register
+from .ipu_layer_drop import IPUHubertEncoder, IPUHubertEncoderStableLayerNorm
 
 
 logger = logging.get_logger(__name__)
@@ -24,8 +26,23 @@ logger = logging.get_logger(__name__)
 
 @register(HubertForSequenceClassification)
 class PipelinedHubertForSequenceClassification(HubertForSequenceClassification, PipelineMixin):
+    def change_hubert_encoder_class(self, restore: bool):
+        """Changes the encoder class to update its forward pass so that it uses our custom version.
+
+        Args:
+            restore: whether to restore the encoder to its original version or not.
+        """
+        if self.config.do_stable_layer_norm:
+            new_cls = HubertEncoderStableLayerNorm if restore else IPUHubertEncoderStableLayerNorm
+        else:
+            new_cls = HubertEncoder if restore else IPUHubertEncoder
+        self.hubert.encoder.__class__ = new_cls
+
     def parallelize(self):
         super().parallelize()
+
+        self.change_hubert_encoder_class(False)
+
         self.hubert.feature_extractor = poptorch.BeginBlock(self.hubert.feature_extractor, ipu_id=0)
         self.hubert.feature_projection = poptorch.BeginBlock(self.hubert.feature_projection, ipu_id=0)
         self.hubert.encoder = poptorch.BeginBlock(self.hubert.encoder, ipu_id=0)
@@ -41,6 +58,14 @@ class PipelinedHubertForSequenceClassification(HubertForSequenceClassification, 
         last_ipu = self.ipu_config.ipus_per_replica - 1
         self.projector = poptorch.BeginBlock(self.projector, ipu_id=last_ipu)
         self.classifier = poptorch.BeginBlock(self.classifier, ipu_id=last_ipu)
+        return self
+
+    def deparallelize(self):
+        """
+        Undo the changes to the model done by `parallelize`.
+        """
+        super().deparallelize()
+        self.change_hubert_encoder_class(True)
         return self
 
     @poptorch.autocast(enabled=True)
