22d65c7b5f2f35585fc6f6956c287a03c20bcaec
Michael Benayoun <mickbenayoun@gmail.com>
2022-08-30 15:26:21 +0200

    Removed unused code in T5

--- optimum/graphcore/models/t5/modeling_t5.py
+++ optimum/graphcore/models/t5/modeling_t5.py
@@ -60,10 +60,6 @@ _NON_REVERSIBLE_TRANSFORMATIONS = [
 class PipelinedT5ForConditionalGeneration(
     GenerationMethodsMixin, T5ForConditionalGeneration, PipelineMixin, IPUGenerationMixin
 ):
-    @property
-    def is_encoder_and_decoder_embeddings_computation_shared(self):
-        return isinstance(self.shared, SharedEmbedding)
-
     def scale_down_weights(self, factor: float = 1, restore: bool = False):
         self.lm_scale_modifier = 1 if not restore else None
         # self.lm_scale_modifier = nn.Parameter(torch.ones(self.config.d_model, dtype=torch.float16)) if not restore else None
@@ -178,126 +174,37 @@ class PipelinedT5ForConditionalGeneration(
         self = composition(self, reverse=True)
         return self
 
-    def forward(
-        self,
-        input_ids: Optional[torch.LongTensor] = None,
-        attention_mask: Optional[torch.FloatTensor] = None,
-        decoder_input_ids: Optional[torch.LongTensor] = None,
-        decoder_attention_mask: Optional[torch.BoolTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
-        decoder_head_mask: Optional[torch.FloatTensor] = None,
-        cross_attn_head_mask: Optional[torch.Tensor] = None,
-        encoder_outputs: Optional[Tuple[Tuple[torch.Tensor]]] = None,
-        past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None,
-        inputs_embeds: Optional[torch.FloatTensor] = None,
-        decoder_inputs_embeds: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> Union[Tuple[torch.FloatTensor], Seq2SeqLMOutput]:
-        use_cache = use_cache if use_cache is not None else self.config.use_cache
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+    # def train(self, mode: bool = True) -> "PipelinedT5ForConditionalGeneration":
+    #     mod = super(T5ForConditionalGeneration, self).train(mode=mode)
+    #     # TODO: enable that once generation is supported.
+    #     # mod.forward = mod._forward_for_train if mode else mod._forward_for_generate
+    #     mod.forward = mod._forward_for_train
+    #     return mod
 
-        if self.is_encoder_and_decoder_embeddings_computation_shared:
-            inputs_embeds, decoder_inputs_embeds = self.shared(
-                input_ids=input_ids,
-                decoder_input_ids=decoder_input_ids,
-            )
-            if inputs_embeds is not None:
-                input_ids = None
-            if decoder_inputs_embeds is not None:
-                decoder_input_ids = None
+    # def _forward_for_train(self, input_ids, attention_mask, decoder_input_ids, labels=None):
+    #     outputs = super().forward(
+    #         input_ids=input_ids,
+    #         attention_mask=attention_mask,
+    #         decoder_input_ids=decoder_input_ids,
+    #         labels=labels,
+    #         use_cache=False,
+    #         return_dict=False,
+    #     )
+    #     # Only returning the loss to make the communication between the host and the device faster.
+    #     return outputs[0:1]
 
-        # FutureWarning: head_mask was separated into two input args - head_mask, decoder_head_mask
-        if head_mask is not None and decoder_head_mask is None:
-            if self.config.num_layers == self.config.num_decoder_layers:
-                warnings.warn(__HEAD_MASK_WARNING_MSG, FutureWarning)
-                decoder_head_mask = head_mask
+    # def _forward_for_generate(self, encoder_outputs, decoder_input_ids, attention_mask, labels=None):
+    #     outputs = super().forward(
+    #         encoder_outputs=encoder_outputs,
+    #         attention_mask=attention_mask,
+    #         decoder_input_ids=decoder_input_ids,
+    #         return_dict=False,
+    #         use_cache=False,
+    #         labels=labels,
+    #     )
+    #     # Only returning the loss (if labels is provided) and the logits.
+    #     if labels is None:
+    #         return outputs[:1]
+    #     return outputs[:2]
 
-        # Encode if needed (training, first prediction pass)
-        if encoder_outputs is None:
-            # Convert encoder inputs in embeddings if needed
-            encoder_outputs = self.encoder(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                inputs_embeds=inputs_embeds,
-                head_mask=head_mask,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
-        elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
-            encoder_outputs = BaseModelOutput(
-                last_hidden_state=encoder_outputs[0],
-                hidden_states=encoder_outputs[1] if len(encoder_outputs) > 1 else None,
-                attentions=encoder_outputs[2] if len(encoder_outputs) > 2 else None,
-            )
-
-        hidden_states = encoder_outputs[0]
-
-        if labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
-            # get decoder inputs from shifting lm labels to the right
-            decoder_input_ids = self._shift_right(labels)
-
-        # Decode
-        decoder_outputs = self.decoder(
-            input_ids=decoder_input_ids,
-            attention_mask=decoder_attention_mask,
-            inputs_embeds=decoder_inputs_embeds,
-            past_key_values=past_key_values,
-            encoder_hidden_states=hidden_states,
-            encoder_attention_mask=attention_mask,
-            head_mask=decoder_head_mask,
-            cross_attn_head_mask=cross_attn_head_mask,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        sequence_output = decoder_outputs[0]
-
-        # Set device for model parallelism
-        if self.model_parallel:
-            self.lm_head = self.lm_head.to(self.encoder.first_device)
-            sequence_output = sequence_output.to(self.lm_head.weight.device)
-
-        if self.config.tie_word_embeddings:
-            # Rescale output before projecting on vocab
-            # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
-            sequence_output = sequence_output * (self.model_dim**-0.5)
-
-        lm_scale_modifier = getattr(self, "lm_scale_modifier", None)
-        if lm_scale_modifier is not None:
-            sequence_output = sequence_output * lm_scale_modifier
-
-        lm_logits = self.lm_head(sequence_output)
-
-        loss = None
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss(ignore_index=-100)
-            loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
-        # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
-
-        # Only returning the loss to make the communication between the host and the device faster.
-        if not return_dict:
-            output = (lm_logits,) + decoder_outputs[1:] + encoder_outputs
-            return (loss,) if labels is not None else output
-
-        if loss is not None:
-            return Seq2SeqLMOutput(
-                loss=loss,
-            )
-        return Seq2SeqLMOutput(
-            loss=loss,
-            logits=lm_logits,
-            past_key_values=decoder_outputs.past_key_values,
-            decoder_hidden_states=decoder_outputs.hidden_states,
-            decoder_attentions=decoder_outputs.attentions,
-            cross_attentions=decoder_outputs.cross_attentions,
-            encoder_last_hidden_state=encoder_outputs.last_hidden_state,
-            encoder_hidden_states=encoder_outputs.hidden_states,
-            encoder_attentions=encoder_outputs.attentions,
-        )
+    # forward = _forward_for_train
