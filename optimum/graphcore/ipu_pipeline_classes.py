37f5cf73c08629b3ff7a32647b2fd3499dca709f
Jinchen Ge <jincheng@graphcore.ai>
2022-09-21 17:22:57 +0100

    Override IPUFillMaskPipeline methods

--- optimum/graphcore/ipu_pipeline_classes.py
+++ optimum/graphcore/ipu_pipeline_classes.py
@@ -16,7 +16,9 @@ from transformers import (
 
 
 class IPUFillMaskPipeline(FillMaskPipeline):
-    def _sanitize_parameters(self, top_k=None, targets=None):
+    def _sanitize_parameters(self, top_k=None, targets=None, **tokenizer_kwargs):
+        preprocess_params = tokenizer_kwargs
+
         postprocess_params = {}
 
         if targets is not None:
@@ -30,12 +32,12 @@ class IPUFillMaskPipeline(FillMaskPipeline):
             raise PipelineException(
                 "fill-mask", self.model.base_model_prefix, "The tokenizer does not define a `mask_token`."
             )
-        return {}, {}, postprocess_params
+        return preprocess_params, {}, postprocess_params
 
-    def preprocess(self, inputs, return_tensors=None, **preprocess_parameters) -> Dict[str, GenericTensor]:
+    def preprocess(self, inputs, return_tensors=None, **tokenizer_kwargs) -> Dict[str, GenericTensor]:
         if return_tensors is None:
             return_tensors = self.framework
-        model_inputs = self.tokenizer(inputs, return_tensors=return_tensors)
+        model_inputs = self.tokenizer(inputs, return_tensors=return_tensors, **tokenizer_kwargs)
         self.ensure_exactly_one_mask_token(model_inputs)
         return model_inputs
 
