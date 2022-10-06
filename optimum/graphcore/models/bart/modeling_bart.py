d571cc6d4ecb65937dcc3e42152da51dd29dfaaf
James Briggs <jamesbr@graphcore.ai>
2022-10-06 22:13:54 +0100

    add docstring to bart

--- optimum/graphcore/models/bart/modeling_bart.py
+++ optimum/graphcore/models/bart/modeling_bart.py
@@ -804,7 +804,7 @@ class PipelinedBartForConditionalGeneration(
             config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
             (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
 
-        Returns:
+        Returns: Tuple or Seq2SeqLMOutput
         """
         outputs = super().forward(
             input_ids,
