c215c56e2fdfad7a320445b240a896aedd7427f5
Jinchen Ge <jincheng@graphcore.ai>
2022-10-10 15:30:32 +0100

    Comment CustomPipeline

--- tests/pipelines/test_pipelines_common.py
+++ tests/pipelines/test_pipelines_common.py
@@ -786,23 +786,23 @@ class PipelineUtilsTest(unittest.TestCase):
         return models_are_equal
 
 
-class CustomPipeline(Pipeline):
-    def _sanitize_parameters(self, **kwargs):
-        preprocess_kwargs = {}
-        if "maybe_arg" in kwargs:
-            preprocess_kwargs["maybe_arg"] = kwargs["maybe_arg"]
-        return preprocess_kwargs, {}, {}
+# class CustomPipeline(Pipeline):
+#     def _sanitize_parameters(self, **kwargs):
+#         preprocess_kwargs = {}
+#         if "maybe_arg" in kwargs:
+#             preprocess_kwargs["maybe_arg"] = kwargs["maybe_arg"]
+#         return preprocess_kwargs, {}, {}
 
-    def preprocess(self, text, maybe_arg=2):
-        input_ids = self.tokenizer(text, return_tensors="pt")
-        return input_ids
+#     def preprocess(self, text, maybe_arg=2):
+#         input_ids = self.tokenizer(text, return_tensors="pt")
+#         return input_ids
 
-    def _forward(self, model_inputs):
-        outputs = self.model(**model_inputs)
-        return outputs
+#     def _forward(self, model_inputs):
+#         outputs = self.model(**model_inputs)
+#         return outputs
 
-    def postprocess(self, model_outputs):
-        return model_outputs["logits"].softmax(-1).numpy()
+#     def postprocess(self, model_outputs):
+#         return model_outputs["logits"].softmax(-1).numpy()
 
 
 # @is_pipeline_test
