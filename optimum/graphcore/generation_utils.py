e3baa6f5485a7f10bb68a3cde47c5aa7fb90eb65
Mwiza Kunda <mwizak@graphcore.ai>
2023-03-21 14:12:46 +0000

    fix: revert docstring

--- optimum/graphcore/generation_utils.py
+++ optimum/graphcore/generation_utils.py
@@ -97,7 +97,7 @@ class DecoderWrapper(nn.Module):
     def forward(self, t, **model_inputs):
         """
         Args:
-            t : (`torch.Tensor(int)`) Tensor of dimension 1 representing the current length of the sequence being generated
+            t : (`torch.Tensor(int)`) Tensor with single int representing the current length of the sequence being generated
             model_inputs : Regular model_inputs passed to the wrapped model.
         Returns:
             The output logits at position `t` only
