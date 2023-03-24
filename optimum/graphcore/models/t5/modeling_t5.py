62ac3035cec063918dccab18ad8b2928b3bbd23a
Harry Mellor <harrym@graphcore.ai>
2023-03-24 17:30:30 +0000

    Fix overflow in masking

--- optimum/graphcore/models/t5/modeling_t5.py
+++ optimum/graphcore/models/t5/modeling_t5.py
@@ -134,21 +134,11 @@ class CustomT5Block(T5Block):
 
 
 class CustomT5Stack(T5Stack):
-    def invert_attention_mask(self, encoder_attention_mask: Tensor) -> Tensor:
-        if encoder_attention_mask.dim() == 3:
-            encoder_extended_attention_mask = encoder_attention_mask[:, None, :, :]
-        if encoder_attention_mask.dim() == 2:
-            encoder_extended_attention_mask = encoder_attention_mask[:, None, None, :]
-        # T5 has a mask that can compare sequence ids, we can simulate this here with this transposition
-        # Cf. https://github.com/tensorflow/mesh/blob/8d2465e9bc93129b913b5ccc6a59aa97abd96ec6/mesh_tensorflow
-        # /transformer/transformer_layers.py#L270
-        # encoder_extended_attention_mask = (encoder_extended_attention_mask ==
-        # encoder_extended_attention_mask.transpose(-1, -2))
-        encoder_extended_attention_mask = encoder_extended_attention_mask.to(dtype=self.dtype)  # fp16 compatibility
+    def invert_attention_mask(self, *args, **kwargs) -> Tensor:
+        return super().invert_attention_mask(*args, **kwargs) * 0.75
 
-        # Always use -1e4 to avoid NaN issues.
-        encoder_extended_attention_mask = (1.0 - encoder_extended_attention_mask) * -1e4
-        return encoder_extended_attention_mask
+    def get_extended_attention_mask(self, *args, **kwargs) -> Tensor:
+        return super().get_extended_attention_mask(*args, **kwargs) * 0.75
 
 
 @register(T5ForConditionalGeneration)
