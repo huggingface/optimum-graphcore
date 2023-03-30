350e7f3a8b1ea9d9d5019fb2f9cfdbf570ccd0d1
Goran Katalinic <gorank@graphcore.ai>
2023-03-30 18:33:52 +0100

    Bump diffusers to 0.12.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except Exception as error:
 INSTALL_REQUIRES = [
     "transformers==4.25.1",
     "optimum==1.6.1",
-    "diffusers[torch]==0.11.1",
+    "diffusers[torch]==0.12.1",
     "datasets",
     "tokenizers",
     "torch @ https://download.pytorch.org/whl/cpu/torch-1.13.1%2Bcpu-cp38-cp38-linux_x86_64.whl",
