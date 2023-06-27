20109688172d469aae2a09f659958082212a42f4
Alexandre Coniasse <alexandrec@graphcore.ai>
2023-06-27 17:36:50 +0100

    Support T5EncoderModel for t5-based embedding models

--- optimum/graphcore/models/t5/__init__.py
+++ optimum/graphcore/models/t5/__init__.py
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .modeling_t5 import PipelinedT5ForConditionalGeneration
+from .modeling_t5 import PipelinedT5ForConditionalGeneration, PipelinedT5EncoderModel
