d7daf35fe81195431b83a2f992edbdbf7611ee83
Arsalan Uddin <arsalanu@graphcore.ai>
2023-06-27 09:28:11 +0100

    use latest env vars

--- notebooks/e5-text-embeddings/config.py
+++ notebooks/e5-text-embeddings/config.py
@@ -4,30 +4,34 @@ import logging
 
 logger = logging.getLogger("e5")
 
+configs = {
+    1: {
+        'inference_ipus_per_replica': 1,
+        'inference_layers_per_ipu': [24],
+        'inference_matmul_proportion': [0.1],
+        'inference_replication_factor': 4
+       },
+    2: {
+        'inference_ipus_per_replica': 2,
+        'inference_layers_per_ipu': [10,14],
+        'inference_matmul_proportion': [0.1,0.1],
+        'inference_replication_factor': 2
+       },
+    4: {
+        'inference_ipus_per_replica': 4,
+        'inference_layers_per_ipu': [3,7,7,7],
+        'inference_matmul_proportion': [0.1,0.15,0.15,0.15],
+        'inference_replication_factor': 1
+       }
+}
+
+
 def get_ipu_config(pod_type, n_ipu, device_iterations, replication_factor=None, random_seed=None):
     ipu_config = IPUConfig.from_pretrained("Graphcore/bert-large-uncased").to_dict()
-
-    configs = {
-        1: {
-            'inference_ipus_per_replica': 1,
-            'inference_layers_per_ipu': [24],
-            'inference_matmul_proportion': [0.1],
-            'inference_replication_factor': 4
-           },
-        2: {
-            'inference_ipus_per_replica': 2,
-            'inference_layers_per_ipu': [10,14],
-            'inference_matmul_proportion': [0.1,0.1],
-            'inference_replication_factor': 2
-           },
-        4: {
-            'inference_ipus_per_replica': 4,
-            'inference_layers_per_ipu': [3,7,7,7],
-            'inference_matmul_proportion': [0.1,0.15,0.15,0.15],
-            'inference_replication_factor': 1
-           }
-    }
-
+    
+    executable_cache_dir = os.getenv("POPLAR_EXECUTABLE_CACHE_DIR", "/tmp/exe_cache/")
+    ipu_config['executable_cache_dir'] = executable_cache_dir
+    
     if n_ipu in configs:
         for conf_name in configs[n_ipu]:
             ipu_config[conf_name] = configs[n_ipu][conf_name]
@@ -52,7 +56,7 @@ def get_ipu_config(pod_type, n_ipu, device_iterations, replication_factor=None, 
     ipu_config['recompute_checkpoint_every_layer'] = False
     ipu_config['replicated_tensor_sharding'] = True
     ipu_config['enable_half_partials'] = True
-
+    
     if random_seed:
         ipu_config['random_seed'] = random_seed 
         torch.manual_seed(random_seed)
