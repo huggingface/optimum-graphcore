2aa779ff283cce42c4f5941375247744cd72ac43
Jim Briggs <jamesbr@graphcore.ai>
2023-07-12 22:36:49 +0100

    Adding missing license headers and copyrights (#454)

--- optimum/graphcore/models/whisper/modeling_whisper.py
+++ optimum/graphcore/models/whisper/modeling_whisper.py
@@ -1,3 +1,4 @@
+# Copyright 2022 The OpenAI Authors and The HuggingFace Inc. team. All rights reserved.
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
