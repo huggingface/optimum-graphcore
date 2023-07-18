cbdeea696c58747fc9bf5604d6a9fa766b2908fe
Goran Katalinic <gorank@graphcore.ai>
2023-07-18 18:22:54 +0100

    Check that the dataset has enough examples to fill a batch when creating the dataloaders in trainer

--- optimum/graphcore/trainer.py
+++ optimum/graphcore/trainer.py
@@ -613,6 +613,30 @@ class IPUTrainer:
         else:
             return RandomSampler(self.train_dataset)
 
+    def _check_dataset_can_fill_batch(self, dataset: torch.utils.data.Dataset, for_inference: bool = False) -> None:
+        replication_factor = (
+            self.ipu_config.inference_replication_factor if for_inference else self.ipu_config.replication_factor
+        )
+        gradient_accumulation_steps = 1 if for_inference else self.ipu_config.gradient_accumulation_steps
+        device_iterations = (
+            self.ipu_config.inference_device_iterations if for_inference else self.ipu_config.device_iterations
+        )
+        micro_batch_size = (
+            self.args.per_device_eval_batch_size if for_inference else self.args.per_device_train_batch_size
+        )
+        global_batch_size = micro_batch_size * replication_factor * gradient_accumulation_steps * device_iterations
+        if len(dataset) < global_batch_size:
+            mode_str = "inference_" if for_inference else ""
+            logger.warning(
+                f"The provided dataset is of length {len(dataset)}, but the total dataset batch size is {global_batch_size}. "
+                f"This batch size is calculated as:\n"
+                f"  per_device_{'eval' if for_inference else 'train'}_batch_size={micro_batch_size}\n"
+                f"* {mode_str}{replication_factor=}\n"
+                f"* {mode_str}{gradient_accumulation_steps=}\n"
+                f"* {mode_str}{device_iterations=}\n"
+                "Please disregard this warning if you believe the dataset is reporting an incorrect length, such as 1."
+            )
+
     def get_train_dataloader(self) -> poptorch.DataLoader:
         """
         Returns the training `poptorch.DataLoader`.
@@ -657,6 +681,8 @@ class IPUTrainer:
             if self.args.dataloader_num_workers
             else combined_batch_size
         )
+
+        self._check_dataset_can_fill_batch(train_dataset, for_inference=False)
 
         return poptorch.DataLoader(
             self.opts,
@@ -713,6 +739,8 @@ class IPUTrainer:
 
         eval_sampler = self._get_eval_sampler(eval_dataset)
 
+        self._check_dataset_can_fill_batch(eval_dataset, for_inference=True)
+
         return poptorch.DataLoader(
             self.eval_opts,
             eval_dataset,
@@ -760,6 +788,8 @@ class IPUTrainer:
             )
 
         test_sampler = self._get_eval_sampler(test_dataset)
+
+        self._check_dataset_can_fill_batch(test_dataset, for_inference=True)
 
         # We use the same batch_size as for eval.
         return poptorch.DataLoader(
